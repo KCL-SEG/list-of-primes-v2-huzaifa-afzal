d4d4c60b81fa96a7d610225058a4645f33a08a09
Huzaifa Afzal <muhammad.h.afzal@kcl.ac.uk>
2022-12-09 13:58:27 +0000

    Complete primes task

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [2] #2 always first prime
+    if number_of_primes <= 0:
+        raise ValueError
+    j = 3
+    while len(list) < number_of_primes:
+        prime = True
+        for i in range (2, j):
+            if j % i == 0:
+                prime = False
+                break
+        if(prime==True):
+            list.append(j)
+        j +=1
     return list
